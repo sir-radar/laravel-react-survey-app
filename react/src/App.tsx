7fb906204b9314eccb51b29e595aede0e06a0bf6
Samson Nwokike <nwokikesamson@gmail.com>
2023-02-27 14:05:50 +0100

    added router, layouts and views

--- react/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-    return <div className="">App.tsx</div>;
-}
-
-export default App;
---
+++ react/src/layouts/GuestLayout.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function GuestLayout() {
+  return <div>GuestLayout</div>;
+}
