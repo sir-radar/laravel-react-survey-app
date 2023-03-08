3b1cecbc4b1c9b93d462d8f4571892e1e98a6544
Samson Nwokike <nwokikesamson@gmail.com>
2023-03-08 03:39:48 +0100

    added other backend routes

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::apiResource('survey', SurveyController::class);
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/survey/get-by-slug/{survey:slug}', [SurveyController::class, 'getBySlug']);
+Route::post('/survey/{survey}/answer', [SurveyController::class, 'storeAnswer']);
